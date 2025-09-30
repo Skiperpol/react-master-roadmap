131c0f17260ff73876fd115dbbfca72984f15c2d
Dawid <skiperpol1@gmail.com>
2025-09-30 12:53:05 +0200

    feat(06-lists-and-keys): add solution for 04-list-manipulation

--- react-and-ecosystem/jsx-drills/playground/src/main.tsx
+++ react-and-ecosystem/jsx-drills/playground/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './exercises/06-lists-and-keys/03-index-as-key-experiment/solution/App'
+import App from './exercises/06-lists-and-keys/04-list-manipulation/solution/App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
