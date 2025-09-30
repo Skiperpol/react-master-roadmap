fe2841aac46dfe43959e22b4152d27b8c5f6343b
Dawid <skiperpol1@gmail.com>
2025-09-30 14:17:39 +0200

    feat(07-jsx-security): add solution for 01-string-with-html

---
+++ react-and-ecosystem/jsx-drills/playground/src/exercises/07-jsx-security/01-string-with-html/solution/App.tsx
@@ -0,0 +1,5 @@
+const App = () => {
+    return <div>{"<strong>Hello!</strong>"}</div>;
+}
+
+export default App
--- react-and-ecosystem/jsx-drills/playground/src/main.tsx
+++ react-and-ecosystem/jsx-drills/playground/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './exercises/06-lists-and-keys/04-list-manipulation/solution/App'
+import App from './exercises/07-jsx-security/01-string-with-html/solution/App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
